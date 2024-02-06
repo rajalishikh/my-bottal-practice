b11fbbee479f9e184c834fe7cb2af5c6275505fc
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-02-06 21:42:12 +0600

    add my practice project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <Name></Name>
+      <Name></Name>
+      <Name></Name>
+      <Name></Name>
+      
+      
+    </>
+  )
+}
+
+
+const Name = () => {
+  const person = 44
+    ;
+  return <h1> My old is { person}</h1>
+}
+  
+
+export default App
